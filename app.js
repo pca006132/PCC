42d46b8fe9e3bd72d09be881dc4620afa8c30d7b
pca006132 <john.lck40@gmail.com>
2017-05-08 21:13:26 +0800

    Added auto check update

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ const runner = require('./JsRunner.js');
 const parser = require('./Parser.js');
 const procedure = require('./Procedure.js');
 const CommandModule = require('./CommandModule.js');
+const https = require('https');
+const package = require('./package.json');
 
 function mkdirSync(dirPath) {
     try {
@@ -25,34 +27,51 @@ function initializeDirectory() {
 
 initializeDirectory();
 
+if (options.autoCheckUpdate)
+    https.get("https://registry.npmjs.org/minecraft-pcc/latest", response => {
+        let data = [];
+        response.setEncoding('utf8');
+        response.on('data', (chunk) => data.push(chunk));
+        response.on('end', () => {
+            let version = JSON.parse(data.join(""));
+            if (version.version != package.version) {
+                console.log("# There is a new update of PCC: " + version.version);
+                console.log("# Would you prefer to update to the latest version? (npm update -g minecraft-pcc)");
+            }
+        });
+        response.on('err', () => console.log("# Sorry, we can't check if there is a new update"));
+    });
+
 let args = process.argv.slice(2);
 if (args.length == 0) {
     console.log("Parameters: (* indicates required)");
     console.log("*1: PCC file name");
     console.log(" others: module names(if absent, parse all modules. If present, parse only those modules)");
-    process.exit();
-}
-let fileName = args[0];
-if (args.length > 1) {
-    CommandModule.setWhiteList(args.slice(1));
-}
+    console.log("-----");
+} else {
+    let fileName = args[0];
+    if (args.length > 1) {
+        CommandModule.setWhiteList(args.slice(1));
+    }
 
 
-fs.readFile(fileName, "utf8", (err, data) =>{
-    if (err)
-        throw err;
-    parser.parse(data);
-    let p = new procedure.Procedure("pcc:test", true, false, false);
-    for (let c of runner.scope.InitCommands) {
-        p.addElement(new parser.Line("NA", c, 0));
-    }
-    for (let c of runner.scope.ComamndBlockCommands) {
-        p.addElement(new parser.Line("NA", c, 0));
-    }
-    for (let c of runner.scope.LastCommands) {
-        p.addElement(new parser.Line("NA", c, 0));
-    }
-    //Avoid empty advancement
-    if (p.content.rewards.length > 1)
-        p.saveToFile(options.out);
-});
+    fs.readFile(fileName, "utf8", (err, data) =>{
+        if (err)
+            throw err;
+        parser.parse(data);
+        let p = new procedure.Procedure("pcc:test", true, false, false);
+        for (let c of runner.scope.InitCommands) {
+            p.addElement(new parser.Line("NA", c, 0));
+        }
+        for (let c of runner.scope.ComamndBlockCommands) {
+            p.addElement(new parser.Line("NA", c, 0));
+        }
+        for (let c of runner.scope.LastCommands) {
+            p.addElement(new parser.Line("NA", c, 0));
+        }
+        //Avoid empty advancement
+        if (p.content.rewards.length > 1)
+            p.saveToFile(options.out);
+    });
+
+}
