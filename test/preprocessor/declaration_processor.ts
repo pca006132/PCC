66af2fe36142f8f9d417d58c422b49494630e6f7
switefaster <2734825077@qq.com>
2018-05-09 21:12:00 +0800

    Start declaration test

---
+++ test/preprocessor/declaration_processor.ts
@@ -0,0 +1,20 @@
+import getDeclarations from '../../src/preprocessor/declaration_processor';
+import Line from '../../src/util/line';
+import {expect} from 'chai';
+import * as linked_list from '../../src/util/linked_list';
+
+describe('declarationProcessor', () => {
+    it('should parse constant with #define pattern', () => {
+        let line = {next: new Line("#define $pca006132 girlClother", 0, "test.pcc", 1)};
+        line.next.before = line;
+        let result = getDeclarations(line);
+        expect(result).to.deep.include({
+            constants: [
+                {
+                    name: /\$pca006132/g,
+                    content: 'girlClother'
+                }
+            ]
+        })
+    })
+})
