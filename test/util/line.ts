19c62bc78ac8d8d9ee94c531f1c271c066a805c9
switefaster <2734825077@qq.com>
2018-05-09 12:43:55 +0800

    Moved insertion test

--- test/util/line.ts
+++ test/util/line.ts
@@ -2,42 +2,6 @@ import {expect} from 'chai';
 import Line from '../../src/util/line';
 
 describe('line test', function () {
-    describe('insertAfter', function () {
-        it('should insert b after a', function () {
-            let a = new Line('a', 0, '', 0);
-            let start = {next: a};
-            a.before = start;
-
-            let b = new Line('b', 0, '', 0);
-            let result = b.insertAfter(a);
-
-            expect(result).to.equal(b);
-            expect(b.before).to.equal(a);
-            expect(a.next).to.equal(b);
-        })
-        it('should insert b between a and c', function () {
-            let a = new Line('a', 0, '', 0);
-            let start = {next: a};
-            a.before = start;
-
-            let c = new Line('c', 0, '', 0);
-            c.insertAfter(a);
-
-            let b = new Line('b', 0, '', 0);
-            let result = b.insertAfter(a);
-
-            expect(result).to.equal(b);
-
-            //check a-b connection
-            expect(b.before).to.equal(a);
-            expect(a.next).to.equal(b);
-
-            //check b-c connection
-            expect(b.next).to.equal(c);
-            expect(c.before).to.equal(b);
-        })
-    })
-
     describe('copy', function () {
         it('should copy "a" with file: a and lineNum: 1', function () {
             let a = new Line('a', 0, ['', 'test'], [0, 233]);
